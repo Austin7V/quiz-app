d7f78170c005eb5f64e7e99dadd1249c85d03941
Sergey Badin <sergeykfg1@me.com>
2026-01-05 11:48:09 +0100

    feat: toggle answer visibility on first card

--- js/index.js
+++ js/index.js
@@ -5,3 +5,13 @@ const bookmarkButton = document.querySelector(".card__bookmark");
 bookmarkButton.addEventListener("click", () => {
   bookmarkButton.classList.toggle("card__bookmark--active");
 });
+
+const answerButton = document.querySelector(".card__toggle");
+const answerText = document.querySelector(".card__answer");
+
+answerButton.addEventListener("click", () => {
+  answerText.classList.toggle("hidden");
+
+  const isHidden = answerText.classList.contains("hidden");
+  answerButton.textContent = isHidden ? "Show Answer" : "Hide Answer";
+});
